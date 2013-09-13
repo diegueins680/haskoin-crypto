94773f4b689b1c8a4a03be895ffe1384f8bca112
cpt-octofu <cpt.octofu@gmail.com>
2013-09-13 21:28:40 +0100

    Adding HMAC DRBG bit length checks

--- src/Haskoin/Crypto/Hash.hs
+++ src/Haskoin/Crypto/Hash.hs
@@ -125,14 +125,15 @@ join512 (a,b) = ((toMod512 a) `shiftL` 256) + (toMod512 b)
 
 {- 10.1.2 HMAC_DRBG with HMAC-SHA256
    http://csrc.nist.gov/publications/nistpubs/800-90A/SP800-90A.pdf 
+   Constants are based on recommentations in Appendix D section 2 (D.2)
 -}
 
 type WorkingState    = (BS.ByteString, BS.ByteString, Word64)
 type AdditionalInput = BS.ByteString
 type ProvidedData    = BS.ByteString
 type EntropyInput    = BS.ByteString
-type PersString      = BS.ByteString
 type Nonce           = BS.ByteString
+type PersString      = BS.ByteString
 
 -- 10.1.2.2 HMAC DRBG Update FUnction
 hmacDRBGUpd :: ProvidedData -> BS.ByteString -> BS.ByteString
@@ -147,25 +148,37 @@ hmacDRBGUpd info k0 v0
 
 -- 10.1.2.3 HMAC DRBG Instantiation
 hmacDRBGNew :: EntropyInput -> Nonce -> PersString -> WorkingState
-hmacDRBGNew seed nonce info = (k1,v1,1)            -- 10.1.2.3.6
+hmacDRBGNew seed nonce info 
+    | (BS.length seed + BS.length nonce) * 8 < 384  = error $
+        "Entropy + nonce input length must be at least 384 bit"
+    | (BS.length seed + BS.length nonce) * 8 > 1000 = error $
+        "Entropy + nonce input length can not be greater than 1000 bit"
+    | BS.length info * 8 > 256  = error $ 
+        "Maximum personalization string length is 256 bit"
+    | otherwise                = (k1,v1,1)         -- 10.1.2.3.6
     where s        = BS.concat [seed, nonce, info] -- 10.1.2.3.1
           k0       = BS.replicate 32 0             -- 10.1.2.3.2
           v0       = BS.replicate 32 1             -- 10.1.2.3.3
-          (k1,v1) = hmacDRBGUpd s k0 v0            -- 10.1.2.3.4
+          (k1,v1)  = hmacDRBGUpd s k0 v0           -- 10.1.2.3.4
 
 -- 10.1.2.4 HMAC DRBG Reseeding
 hmacDRBGRsd :: WorkingState -> EntropyInput -> AdditionalInput -> WorkingState
-hmacDRBGRsd (k,v,_) xseed info = (k0,v0,1)  -- 10.1.2.4.4
-    where s        = xseed `BS.append` info -- 10.1.2.4.1
-          (k0,v0) = hmacDRBGUpd s k v       -- 10.1.2.4.2
+hmacDRBGRsd (k,v,_) seed info 
+    | BS.length seed * 8 < 256 = error $ 
+        "Entropy input length must be at least 256 bit"
+    | BS.length seed * 8 > 1000 = error $ 
+        "Entropy input length can not be greater than 1000 bit"
+    | otherwise   = (k0,v0,1)             -- 10.1.2.4.4
+    where s       = seed `BS.append` info -- 10.1.2.4.1
+          (k0,v0) = hmacDRBGUpd s k v     -- 10.1.2.4.2
 
 -- 10.1.2.5 HMAC DRBG Generation
 hmacDRBGGen :: WorkingState -> Word64 -> AdditionalInput
             -> (WorkingState, Maybe BS.ByteString)
 hmacDRBGGen (k0,v0,c0) bytes info 
-    -- 10.1 Table 2 (reseed interval)
-    | c0 > 2^48 = ((k0,v0,c0), Nothing)  -- 10.1.2.5.1
-    | otherwise = ((k2,v3,c1), Just res) -- 10.1.2.5.8
+    | bytes * 8 > 7500 = error "Maximum bits per request is 7500"
+    | c0 > 10000       = ((k0,v0,c0), Nothing)  -- 10.1.2.5.1
+    | otherwise        = ((k2,v3,c1), Just res) -- 10.1.2.5.8
     where (k1,v1) | BS.null info = (k0,v0) 
                   | otherwise    = hmacDRBGUpd info k0 v0   -- 10.1.2.5.2
           (tmp,v2) = go (fromIntegral bytes) k1 v1 BS.empty -- 10.1.2.5.3/4
