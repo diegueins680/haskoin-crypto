74d952c02ef5b1af5d446cfdb76972f0eabc4482
cpt-octofu <cpt.octofu@gmail.com>
2013-12-10 01:22:49 +0000

    Documenting ECDSA curve name

--- Network/Haskoin/Crypto/Curve.hs
+++ Network/Haskoin/Crypto/Curve.hs
@@ -6,6 +6,8 @@ module Network.Haskoin.Crypto.Curve
 , integerA
 ) where
 
+-- SECP256k1 curve parameters
+
 pairG :: (Integer, Integer)
 pairG = ( 0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798       
         , 0X483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8 
