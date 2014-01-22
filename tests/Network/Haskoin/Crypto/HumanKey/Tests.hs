3431dae33623b946c14d43059971f791aad464c8
Jean-Pierre Rupp <root@xeno-genesis.com>
2014-01-22 09:41:38 +0000

    New test for double 64-bit key in RFC-1751.

--- tests/Network/Haskoin/Crypto/HumanKey/Tests.hs
+++ tests/Network/Haskoin/Crypto/HumanKey/Tests.hs
@@ -8,11 +8,21 @@ import Test.Framework
 import Test.Framework.Providers.QuickCheck2
 
 tests :: [Test]
-tests = [ testProperty "Encode/Decode RFC-1751 keys" decodeEncode ]
+tests =
+    [ testGroup "RFC-1751"
+        [ testProperty "Encode/Decode RFC-1751 keys" decodeEncode
+        , testProperty "Double 64-bit key" doubleWord64
+        ]
+    ]
 
 decodeEncode :: (Word64, Word64) -> Bool
 decodeEncode (w1, w2) = bs == bs'
     where bs = encode w1 `C.append` encode w2
-          hk  = decode bs :: HumanKey
+          hk = decode bs :: HumanKey
           bs' = encode hk
 
+doubleWord64 :: Word64 -> Bool
+doubleWord64 w = ws1 == ws2
+    where bs = encode w `C.append` encode w
+          HumanKey ws = decode bs :: HumanKey
+          (ws1, ws2) = splitAt 6 $ words ws
